8c5d7fe87bcae2e0d6af5df3fd905e45b9da8c89
septimochen <qizu.chen@hotmail.com>
2020-10-08 09:34:56 +0000

    update closures captures

--- src/functions/captures.rs
+++ src/functions/captures.rs
@@ -12,11 +12,28 @@ fn move_test() {
 
 #[test]
 fn capture_test() {
-    // use std::mem;
+    use std::mem;
     let color = String::from("green");
     let print = || { println!("'color:' {}", color)};
     print();
 
     let _reborrow = &color;
     print();
+
+    let _color_moved = color;
+
+    let mut count = 0;
+    let mut inc = || {
+        count += 1;
+        println!("count: {}", count);
+    };
+    inc();
+    inc();
+
+    let movable = Box::new(3);
+    let consume = || {
+        println!("movable: {:?}", movable);
+        mem::drop(movable);
+    };
+    consume();
 }
