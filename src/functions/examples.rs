3ad30a9d73047d7bae41bc13dc4885f0e7ba0d7f
septimochen <qizu.chen@hotmail.com>
2020-10-09 06:26:00 +0000

    update closures examples

--- src/functions/examples.rs
+++ src/functions/examples.rs
@@ -3,6 +3,13 @@ pub fn examples_std() {
     let vec1 = vec![1, 2, 3];
     let vec2 = vec![3, 4, 5];
 
+    // `iter()` for vecs yields `&i32`. Destructure to `i32`.
     assert_eq!(vec1.iter().any(|&x| x == 2), true);
+    // `into_iter()` for vecs yields `i32`. No destructuring required.
     assert_eq!(vec2.into_iter().any(|x| x == 3), true);
+
+    let array1 = [1, 2, 3];
+    let _array2 = [3, 4, 5];
+    assert_eq!(array1.iter().any(|&x| x == 2), true);
+    // assert_eq!(array2.into_iter().any(|&x| x == 2), false);
 }
