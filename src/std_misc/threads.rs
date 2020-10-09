f7a7fed8f6a2b412cf59fa720c921f6bd8018bde
septimochen <qizu.chen@hotmail.com>
2020-10-09 14:44:48 +0000

    update std misc

---
+++ src/std_misc/threads.rs
@@ -0,0 +1,16 @@
+#[test]
+pub fn threads_test() {
+    use std::thread;
+    static NTHREADS: i32 = 10; 
+
+    let mut children = vec![];
+    for i in 0..NTHREADS {
+        children.push(thread::spawn(move || {
+            println!("this is thread number {}", i);
+        }))
+    }
+
+    for child in children {
+        let _ = child.join();
+    }
+}
