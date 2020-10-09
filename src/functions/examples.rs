d8f8f1e957d1bb4249404f45088d4b215a2f819e
septimochen <qizu.chen@hotmail.com>
2020-10-09 06:20:11 +0000

    update closures examples

--- src/functions/examples.rs
+++ src/functions/examples.rs
@@ -1,4 +1,8 @@
 #[test]
 pub fn examples_std() {
-    
+    let vec1 = vec![1, 2, 3];
+    let vec2 = vec![3, 4, 5];
+
+    assert_eq!(vec1.iter().any(|&x| x == 2), true);
+    assert_eq!(vec2.into_iter().any(|x| x == 3), true);
 }
