4a54b51754ed584ecf0f4db1401ae6676a55afef
septimochen <qizu.chen@hotmail.com>
2020-10-10 07:23:10 +0000

    update std misc map

--- src/std_misc/map_reduce.rs
+++ src/std_misc/map_reduce.rs
@@ -4,6 +4,21 @@ pub fn map_test() {
     let data = vec![1, 2, 3];
 
     // see Struct std::iter::Map
-    let b: Vec<i32> = data.into_iter().map(|x| x * 2).collect();
+    let b: Vec<i32> = data.clone().into_iter().map(|x| x * 2).collect();
+    let s: i32 = data.clone().into_iter().map(|x| x * x).sum();
     println!("{:?}", b);
+    println!("{}", s);
+
+    let mut c = 0;
+
+    for pair in vec!['a', 'b', 'c']
+        .into_iter()
+        .map(|letter| {
+            c += 1;
+            (letter, c)
+        })
+        .rev()
+    {
+        println!("{:?}", pair);
+    }
 }
