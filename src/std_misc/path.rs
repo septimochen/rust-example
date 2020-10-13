dbacbaa8fb8f38ff7a3f7097d5c01e1b500f0fa3
septimochen <qizu.chen@hotmail.com>
2020-10-13 15:37:52 +0000

    path

--- src/std_misc/path.rs
+++ src/std_misc/path.rs
@@ -4,4 +4,10 @@ pub fn path_test() {
     let path = Path::new(".");
     let _display = path.display();
     println!("{}", _display);
+
+    let new_path = path.join("a").join("b");
+    match new_path.to_str() {
+        None => panic!("new path is not valid utf-8 sequence!"),
+        Some(s) => println!("new path is {}", s),
+    }
 }
