10eadd2522d5dff2861596996d922406ee978e75
septimochen <qizu.chen@hotmail.com>
2020-10-14 13:03:41 +0000

    file io

--- src/std_misc/file_io.rs
+++ src/std_misc/file_io.rs
@@ -5,15 +5,15 @@ pub fn file_open() {
     use std::path::Path;
 
     let path = Path::new("src/std_misc/hello.txt");
-    let display = path.display();
+    let display = path.to_str();
 
     let mut file = match File::open(&path) {
-        Err(why) => panic!("couldn't open {}: {}", display, why),
+        Err(why) => panic!("couldn't open {:?}: {}", display, why),
         Ok(file) => file,
     };
     let mut s = String::new();
     match file.read_to_string(&mut s) {
-        Err(why) => panic!("couldn't read {}: {}", display, why),
-        Ok(_) => println!("{} contains: \n{}", display, s),
+        Err(why) => panic!("couldn't read {:?}: {}", display, why),
+        Ok(_) => println!("{:?} contains: \n{}", display, s),
     }
 }
