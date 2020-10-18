dbc22164ab3d0d19f091bc0d45c663f86f071692
septimochen <qizu.chen@hotmail.com>
2020-10-18 09:09:41 +0000

    child pipe

--- src/std_misc/child_process.rs
+++ src/std_misc/child_process.rs
@@ -31,4 +31,22 @@ pub fn pipe_test() {
     use std::process::{Command, Stdio};
 
     static PANGRAM: &'static str = "the quick brown fox jumped over the lazy dog\n";
+    let process = match Command::new("wc")
+        .stdin(Stdio::piped())
+        .stdout(Stdio::piped())
+        .spawn()
+    {
+        Err(why) => panic!("couldn't spawn wc: {}", why),
+        Ok(process) => process,
+    };
+    match process.stdin.unwrap().write_all(PANGRAM.as_bytes()) {
+        Err(why) => panic!("couldn't write to stdin: {}", why),
+        Ok(_) => println!("sent pangram to wc"),
+    }
+
+    let mut s = String::new();
+    match process.stdout.unwrap().read_to_string(&mut s) {
+        Err(why) => panic!("couldn't read stdout {}", why),
+        Ok(_) => println!("wc responded with:\n{}", s),
+    }
 }
